8624ef52af3f33541cfa05e4ef9fb4b73520282a
vitali <vitali@gmail.com>
2024-04-26 12:40:37 +0300

    init

--- modules/aws-network/01-main.tf
+++
@@ -1,9 +0,0 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
-  default_tags {
-    tags = {
-      "kubernetes.io/cluster/kubernetes" = "owned"
-    }
-  }
-}
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cluster_endpoint" {
-  description = "Cluster endpoint"
-  value       = module.k8s_cluster.cluster_endpoint
-}
